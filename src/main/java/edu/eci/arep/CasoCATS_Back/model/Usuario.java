1a74a91bbc9b94a7e134f57609fe64526178871a
Sebastian <sebastian.goenaga@hotmail.com>
2019-05-21 08:21:23 -0500

    proyecto

--- src/main/java/edu/eci/arep/CasoCATS_Back/model/Usuario.java
+++ src/main/java/edu/eci/arep/CasoCATS_Back/model/Usuario.java
@@ -1,6 +1,7 @@
 package edu.eci.arep.CasoCATS_Back.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Usuario implements Serializable  {
 	
@@ -11,6 +12,21 @@ public class Usuario implements Serializable  {
 	
 	private String firstName, lastName, phoneNumeber, password, email;
 	private long cedula;
+	private List<Credito> creditos;
+	public List<Credito> getCreditos() {
+		return creditos;
+	}
+	public void setCreditos(List<Credito> creditos) {
+		this.creditos = creditos;
+	}
+	public CuentaAhorro getCuentaAhorro() {
+		return cuentaAhorro;
+	}
+	public void setCuentaAhorro(CuentaAhorro cuentaAhorro) {
+		this.cuentaAhorro = cuentaAhorro;
+	}
+	private CuentaAhorro cuentaAhorro;
+	
 	public String getFirstName() {
 		return firstName;
 	}
--- src/main/java/edu/eci/arep/CasoCATS_Back/persistences/repositories/IUserRepository.java
+++ src/main/java/edu/eci/arep/CasoCATS_Back/persistences/repositories/IUserRepository.java
@@ -1,19 +1,20 @@
 package edu.eci.arep.CasoCATS_Back.persistences.repositories;
 
 import java.sql.SQLException;
-
-import javax.sql.DataSource;
+import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
 import edu.eci.arep.CasoCATS_Back.model.*;
 
 @Repository
-public interface IUserRepository extends DAO {
-	Usuario getUserByEmail(String email) throws SQLException;
+public interface IUserRepository extends DAO<Usuario, Long>{
+	Usuario getUserByCedula(long cedula) throws SQLException; 
 
 	Usuario getUserById(long idUser) throws SQLException;
-	void updateJairitos(String email,int value) throws SQLException;
-	void updateCongelados(String email,int value) throws SQLException;
+	
+	List<Credito> getCreditosUserCedula(long cedula);
+	
+	
 
 }
